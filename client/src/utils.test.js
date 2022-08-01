fadb5add39a99ebd564b7c67af6eb1ba24700daa
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-08-01 15:08:12 +1200

    finish off main happy path test for the getLetterGrade utility

--- client/src/utils.test.js
+++ client/src/utils.test.js
@@ -137,6 +137,54 @@ const gradeScale = [
 
 describe("getLetterGrade", () => {
   test.todo("Test happy path scenarios");
+
+  it("should return a letter as a string", () => {
+    const result = getLetterGrade("A+", 5, 98);
+    expect(typeof result === "string").toBe(true);
+  });
+
+  it("should test that a mark is greater than or equal to the minMark, and less than or equal to the maxMark", () => {
+    const result = getLetterGrade([0], 5, 100);
+    expect(result).toBe("A+");
+  });
+
+  it.each([
+    [gradeScale, "Level 5", 95],
+    [gradeScale, "Level 5", 87],
+    [gradeScale, "Level 5", 83],
+    [gradeScale, "Level 5", 77],
+    [gradeScale, "Level 5", 72],
+    [gradeScale, "Level 5", 67],
+    [gradeScale, "Level 5", 62],
+    [gradeScale, "Level 5", 57],
+    [gradeScale, "Level 5", 52],
+    [gradeScale, "Level 5", 45],
+    [gradeScale, "Level 5", 35],
+    [gradeScale, "Level 6", 95],
+    [gradeScale, "Level 6", 89],
+    [gradeScale, "Level 6", 82],
+    [gradeScale, "Level 6", 77],
+    [gradeScale, "Level 6", 72],
+    [gradeScale, "Level 6", 67],
+    [gradeScale, "Level 6", 62],
+    [gradeScale, "Level 6", 57],
+    [gradeScale, "Level 6", 52],
+    [gradeScale, "Level 6", 45],
+    [gradeScale, "Level 6", 35],
+  ])(
+    "should return a the correct 'Grade' when a valid 'Level' and a valid 'Mark' are entered",
+    (gradeScale, level, mark) => {
+      const result = getLetterGrade(gradeScale, level, mark);
+      const expectedResult = gradeScale.filter(
+        (singleGrade) =>
+          level === singleGrade.level &&
+          mark >= singleGrade.minMark &&
+          mark <= singleGrade.maxMark
+      );
+      expect(result).toEqual(expectedResult.map((newGrade) => newGrade.grade));
+    }
+  );
+
   test.todo("Test negative scenarios");
 
   it("Throws an error for invalid gradeScale", () => {
@@ -147,50 +195,10 @@ describe("getLetterGrade", () => {
       "Grade scale not provided."
     );
   });
-  it.each([
-    ["A+", 5, 95],
-    ["A", 5, 87],
-    ["A-", 5, 83],
-    ["B+", 5, 77],
-    ["B", 5, 72],
-    ["B-", 5, 67],
-    ["C+", 5, 62],
-    ["C", 5, 57],
-    ["C-", 5, 52],
-    ["D", 5, 45],
-    ["E", 5, 35],
-    ["A+", 6, 95],
-    ["A", 6, 89],
-    ["A-", 6, 82],
-    ["B+", 6, 77],
-    ["B", 6, 72],
-    ["B-", 6, 67],
-    ["C+", 6, 62],
-    ["C", 6, 57],
-    ["C-", 6, 52],
-    ["D", 6, 45],
-    ["E", 6, 35],
-  ])(
-    "should return %s when a valid %s and %s are entered",
-    (letterGrade, level, mark) => {
-      const result = getLetterGrade(gradeScale, level, mark);
-      expect(result).toBe(letterGrade);
-    }
-  );
 
   it("should return an error message when invalid data type is used in input", () => {
     expect(() => getLetterGrade(4, "blah", "blah")).toThrow(
       "Invalid input: Input must be a valid gradeScale, mark and level"
     );
-  });
-
-  it("should return a letter as a string", () => {
-    const result = getLetterGrade("A+", 5, 98);
-    expect(typeof type === "string").toBe(true);
-  });
-
-  it("should test that a mark is greater than or equal to the minMark, and less than or equal to the maxMark", () => {
-    const result = getLetterGrade([0], 5, 100);
-    expect(result).toBe("A+");
   });
 });
