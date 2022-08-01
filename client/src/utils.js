d471d104db4e89f64830b96d597079791a2b83df
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-08-01 15:28:40 +1200

    write the logic for the getLetterGrade utility function and have main happy path tests passing

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,16 @@
-const getLetterGrade = (gradeScale, mark, level) => {
+const getLetterGrade = (gradeScale, level, mark) => {
   if (!Array.isArray(gradeScale) || !gradeScale.length) {
     throw new Error("Grade scale not provided.");
   }
+
+  const letterGrade = gradeScale.filter(
+    (singleGrade) =>
+      level === singleGrade.level &&
+      mark >= singleGrade.minMark &&
+      mark <= singleGrade.maxMark
+  );
+
+  return letterGrade.map((newGrade) => newGrade.grade);
 };
 
 module.exports = { getLetterGrade };
