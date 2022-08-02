1e779a6cd7320524885f520f31e480baaeac4fa7
Ayrton Campbell-Reinheimer <ayrton.campbell-reinheimer@developersinstitute.co.nz>
2022-08-03 09:45:15 +1200

    get swagger test working and returning correct data which is formatted as per API

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,26 +1,22 @@
 const getLetterGrade = (gradeScale, level, mark) => {
-  try {
-    if (!Array.isArray(gradeScale) || !gradeScale.length) {
-      throw new Error("Grade scale not provided.");
-    }
-    if (level === "Level 5" || level === "Level 6") {
-      if (mark >= 0 && mark <= 100) {
-        const filterFunction = gradeScale.filter(
-          (singleGrade) =>
-            level === singleGrade.level &&
-            mark >= singleGrade.minMark &&
-            mark <= singleGrade.maxMark
-        );
-        const result = filterFunction.map((newGrade) => newGrade.grade);
-        return result.toString();
-      } else {
-        throw new Error("Mark must be valid: A Number between 0 and 100");
-      }
+  if (!Array.isArray(gradeScale) || !gradeScale.length) {
+    throw new Error("Grade scale not provided.");
+  }
+  if (level === "Level 5" || level === "Level 6") {
+    if (mark >= 0 && mark <= 100) {
+      const filterFunction = gradeScale.filter(
+        (singleGrade) =>
+          level === singleGrade.level &&
+          mark >= singleGrade.minMark &&
+          mark <= singleGrade.maxMark
+      );
+      const result = filterFunction.map((newGrade) => newGrade.grade);
+      return result.toString();
     } else {
-      throw new Error("Level must only be either Level 5 or Level 6");
+      throw new Error("Mark must be valid: A Number between 0 and 100");
     }
-  } catch (error) {
-    throw Error(error);
+  } else {
+    throw new Error("Level must only be either Level 5 or Level 6");
   }
 };
 
